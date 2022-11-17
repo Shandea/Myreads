929d83cae3e3a1accca93def707f4cf6dcd82388
Shandea <86807815+Shandea@users.noreply.github.com>
2022-11-17 10:20:40 -0600

    updated components

---
+++ src/components/Book.js
@@ -0,0 +1,52 @@
+const Book = ({ book, index, onUpdateBook }) => {
+  return (
+    <li key={index}>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 192,
+              backgroundImage: `url(${book.imageLinks.thumbnail})`,
+            }}
+          ></div>
+          <div className="book-shelf-changer">
+            <select onChange={(e) => onUpdateBook(e, book)}>
+            <option value="">
+               Move Too..
+              </option>
+              <option value="None">
+                None
+              </option>
+              <option value="currentlyReading">
+                Currently Reading
+              </option>
+              <option value="wantToRead">
+                Want To Read
+              </option>
+              <option value="read">
+                Read
+              </option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">
+          {book.authors?.map((author, i) => {
+            if (i === book.authors.length - 1) {
+              return `${author}`;
+            } else {
+              return `${author},`;
+            }
+          })}
+        </div>
+      </div>
+    </li>
+  );
+
+  // return (
+  //  <div> {props.book.title} </div>
+};
+
+export default Book;
---
+++ src/components/Bookshelf.js
@@ -0,0 +1,45 @@
+import Book from "./Book";
+
+const Bookshelf = ({shelf, collection, onUpdateBook}) => {
+  return (
+      <div className="bookshelf">
+                <h2 className="bookshelf-title">{shelf}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+{
+      shelf === "Currently Reading" ? 
+      collection.map((book, i) => (
+        book.shelf ==="currentlyReading" ? 
+        <Book key= {i} book={book} index={i} onUpdateBook={onUpdateBook} />
+        : null 
+      ))
+      :shelf === "Want to Read" ? 
+      collection.map((book, i) => (
+        book.shelf ==="wantToRead" ? 
+        <Book key= {i} book={book} index={i} onUpdateBook={onUpdateBook} />
+        : null 
+      ))
+       :shelf === "Read" ? 
+       collection.map((book, i) => (
+         book.shelf ==="read" ? 
+         <Book key= {i} book={book} index={i} onUpdateBook={onUpdateBook} />
+         : null 
+       ))
+       :null
+}
+       </ol> 
+       </div>
+       </div>
+    // // <ol>
+    //   {/* {props.books.map((book) => (
+    //     <li key={book.id}>
+    //       <Book book = {book} />
+    //     </li>
+    //   ))}
+    // </ol> */}
+  
+  );
+
+};
+
+export default Bookshelf;
